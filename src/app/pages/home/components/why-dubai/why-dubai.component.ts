5b03ed6e018c22973f6ae2659d9755af96d267a5
Uktra <uktra.ds@gmail.com>
2025-03-09 23:58:55 +0200

    Add animation for why-dubai section

--- src/app/pages/home/components/why-dubai/why-dubai.component.ts
+++ src/app/pages/home/components/why-dubai/why-dubai.component.ts
@@ -1,9 +1,19 @@
 import { AsyncPipe, SlicePipe } from '@angular/common';
 import { WhyDubaiService } from './../../../../core/services/why-dubai/why-dubai.service';
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { Observable } from 'rxjs';
 import { GlobalBtnComponent } from '../../../../layouts/global-btn/global-btn.component';
 import { TranslateModule } from '@ngx-translate/core';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-why-dubai',
@@ -11,8 +21,10 @@ import { TranslateModule } from '@ngx-translate/core';
   templateUrl: './why-dubai.component.html',
   styleUrl: './why-dubai.component.scss',
 })
-export class WhyDubaiComponent implements OnInit {
+export class WhyDubaiComponent implements OnInit, AfterViewInit {
   dubaiTodos$!: Observable<{ todos: { todo: string }[] }>;
+  @ViewChild('mainContent', { static: true }) mainContent!: ElementRef;
+
   constructor(private whyDubaiService: WhyDubaiService) {}
 
   dubaiAdvantages: string[] = [
@@ -25,5 +37,58 @@ export class WhyDubaiComponent implements OnInit {
 
   ngOnInit(): void {
     this.dubaiTodos$ = this.whyDubaiService.getDubaiAdvantages();
+  }
+
+  ngAfterViewInit(): void {
+    this.animateElements();
+  }
+
+  animateElements() {
+    const article = this.mainContent.nativeElement;
+
+    gsap.from(article.querySelector('.header h1'), {
+      opacity: 0,
+      y: -50,
+      duration: 1,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: article,
+        start: 'top 70%',
+      },
+    });
+
+    gsap.from(article.querySelector('.card'), {
+      opacity: 0,
+      y: 50,
+      duration: 1,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: article,
+        start: 'top 70%',
+      },
+    });
+
+    gsap.from(article.querySelectorAll('.some-details .detail'), {
+      opacity: 0,
+      y: 30,
+      stagger: 0.3,
+      duration: 1,
+      ease: 'power2.out',
+      scrollTrigger: {
+        trigger: article,
+        start: 'top 20%',
+      },
+    });
+
+    gsap.from(article.querySelector('app-global-btn'), {
+      opacity: 0,
+      scale: 0.8,
+      duration: 1,
+      ease: 'back.out(1.5)',
+      scrollTrigger: {
+        trigger: article,
+        start: 'top 70%',
+      },
+    });
   }
 }
